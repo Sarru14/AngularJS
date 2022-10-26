36a81abeea1bca7531a78fd0a1d2c28898de6560
Francesco Boasso <f.boasso@reply.it>
2022-10-26 14:16:46 +0200

    Creata Repo

---
+++ src/directives/home/home.directive.ts
@@ -0,0 +1,22 @@
+angular.module("weatherApp").directive("home", function () {
+  return {
+    restrict: "E",
+    templateUrl: "../src/directives/home/home.html",
+    replace: true,
+    scope: {},
+    controller: [
+      "$scope",
+      "cityService",
+      function homeController($scope, cityService) {
+        
+        let vm = this;
+        vm.city = cityService.city;
+
+        $scope.$watch("$ctrl.city", function () {
+          cityService.city = vm.city;
+        });        
+      },
+    ],
+    controllerAs: "$ctrl",
+  };
+});
